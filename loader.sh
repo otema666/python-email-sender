d38693dba72a5880de95e4848a5961286795de1e
otema666 <otema000@gmail.com>
2023-08-15 02:24:04 +0200

    update loader and create menu

--- loader.sh
+++ loader.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 echo "Checking installed packages..."
 
-if pip list | grep -i "selenium" > /dev/null 2>&1; then
-    echo "selenium OK"
+if pip list | grep -i "requests" > /dev/null 2>&1; then
+    echo "requests OK"
 else
-    echo "Installing selenium..."
-    pip install --disable-pip-version-check selenium > /dev/null 2>&1
+    echo "Installing requests..."
+    pip install --disable-pip-version-check requests > /dev/null 2>&1
+fi
+
+if pip list | grep -i "names" > /dev/null 2>&1; then
+    echo "names OK"
+else
+    echo "Installing names..."
+    pip install --disable-pip-version-check names > /dev/null 2>&1
 fi
 
 if pip list | grep -i "qrcode" > /dev/null 2>&1; then
@@ -30,4 +37,30 @@ else
 fi
 
 echo "All installations complete."
-python main.py
+
+while true; do
+    clear
+    echo "Choose an option:"
+    echo "1. Run main.py"
+    echo "2. Run gui.py"
+    echo "3. Exit"
+    
+    read -p "Enter option number: " choice
+    
+    case $choice in
+        1)
+            python3 main.py
+            ;;
+        2)
+            python3 gui.py
+            ;;
+        3)
+            exit 0
+            ;;
+        *)
+            echo "Invalid option. Please try again."
+            ;;
+    esac
+done
+
+python3 main.py
